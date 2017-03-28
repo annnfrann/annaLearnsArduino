6707719f9948d8febc909f7078183e76157c83f4
Anna Lewis <anna.frances.lewis@gmail.com>
2017-03-28 14:29:04 -0600

    initial commit

---
+++ trafficLight/trafficLight.ino
@@ -0,0 +1,32 @@
+
+int  red = 13;
+int yellow = 12;
+int green = 11;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(red, HIGH);
+  delay(500);
+  digitalWrite(red, LOW);
+  delay(10);
+
+  digitalWrite(yellow, HIGH);
+  delay(500);
+  digitalWrite(yellow, LOW);
+  delay(10);
+
+  digitalWrite(green, HIGH);
+  delay(500);
+  digitalWrite(green, LOW);
+  delay(10);
+}
+
